62737b9cabb50112cc01b006ee562878d44669d7
sashamartian <sasha.martian@yandex.ru>
2017-04-17 15:46:02 +0700

    com.javarush.task.task01.task0101 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -7,6 +7,6 @@ package com.javarush.task.task01.task0101;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        System.out.println("Круто быть программистом!");
+        System.out.println("Я думаю, быть программистом - это круто");
     }
 }
