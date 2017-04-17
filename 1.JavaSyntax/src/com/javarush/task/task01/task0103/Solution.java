1dc28996cf23b262404dbbf9658a911f52547c1d
sashamartian <sasha.martian@yandex.ru>
2017-04-17 15:56:49 +0700

    com.javarush.task.task01.task0103 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -7,5 +7,6 @@ package com.javarush.task.task01.task0103;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("3118");
     }
 }
