5c206606258991d62ebf9c23d8fa1a3f7abcab13
sashamartian <sasha.martian@yandex.ru>
2017-04-17 15:55:01 +0700

    com.javarush.task.task01.task0102 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -7,5 +7,10 @@ package com.javarush.task.task01.task0102;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
     }
 }
