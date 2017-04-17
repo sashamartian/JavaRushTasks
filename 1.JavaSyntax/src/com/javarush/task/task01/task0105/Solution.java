35a87210b9073a36d30ee8ec2e63bb3817748613
sashamartian <sasha.martian@yandex.ru>
2017-04-17 15:58:15 +0700

    com.javarush.task.task01.task0105 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -7,5 +7,8 @@ package com.javarush.task.task01.task0105;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        String name;
+        int age;
+        String city;
     }
 }
