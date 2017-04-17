62326584464a535f75cc1acb4be18b24ef45d6ea
sashamartian <sasha.martian@yandex.ru>
2017-04-17 17:07:49 +0700

    com.javarush.task.task01.task0108 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
@@ -6,20 +6,20 @@ package com.javarush.task.task01.task0108;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "23";
+        //String s = "23";
         int a = 3, b = 2;
-        String four = "четыре";
-        String three = "3";
+        //String four = "четыре";
+        //String three = "3";
 
-        System.out.print("два");
-        System.out.print(" плюс ");
-        System.out.print(s);
+        //System.out.print("два");
+        //System.out.print(" плюс ");
+        //System.out.print(s);
         System.out.print(b);
         System.out.print(" плюс ");
-        System.out.print("три");
+        //System.out.print("три");
         System.out.print(a);
         System.out.print(" равно ");
-        System.out.print("пять");
+        //System.out.print("пять");
         System.out.print(a + b);
     }
 }
