2b76d062db84e1c501da548243276ba7ff51e617
sashamartian <sasha.martian@yandex.ru>
2017-04-18 12:49:45 +0700

    com.javarush.task.task01.task0116 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0116/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0116/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task01.task0116;
 
 public class Solution {
     public static void main(String[] args) {
-        String name = "Elly";
+        String name = "Amigo";
         String text = "Hello " + name + "!";
         System.out.println(text);
     }
