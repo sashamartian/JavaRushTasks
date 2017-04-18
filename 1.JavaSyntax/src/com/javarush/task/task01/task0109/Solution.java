75ad6fa597d520553fa92b98d38e3f500128d3f0
sashamartian <sasha.martian@yandex.ru>
2017-04-18 12:16:26 +0700

    com.javarush.task.task01.task0109 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -7,5 +7,7 @@ package com.javarush.task.task01.task0109;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int a = 0;
+        int b = 1;
     }
 }
