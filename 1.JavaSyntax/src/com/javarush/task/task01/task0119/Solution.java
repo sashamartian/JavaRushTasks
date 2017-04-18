d7ac137544af063e68a8468351f24790a95f684f
sashamartian <sasha.martian@yandex.ru>
2017-04-18 13:34:33 +0700

    com.javarush.task.task01.task0119 Done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0119/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0119/Solution.java
@@ -7,7 +7,7 @@ package com.javarush.task.task01.task0119;
 public class Solution {
     public static void main(String[] args) {
         String name = "Amigo";
-        int age = 2;
+        int age = 15;
         int money = 500;
         int weight = 70;
     }
