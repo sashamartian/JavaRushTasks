95764c4017b670fdca36eeea1965c2a4ec8414d0
sashamartian <sasha.martian@yandex.ru>
2017-04-18 19:10:56 +0700

    com.javarush.task.task02.task0204 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0204/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0204/Solution.java
@@ -6,6 +6,10 @@ package com.javarush.task.task02.task0204;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
     }
 
     public static class Man {
