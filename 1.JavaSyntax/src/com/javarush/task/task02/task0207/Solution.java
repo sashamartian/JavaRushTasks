e894e1a98a4a49c96ced74c926b0152a596c186a
sashamartian <sasha.martian@yandex.ru>
2017-04-18 19:19:53 +0700

    com.javarush.task.task02.task0207 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
@@ -6,5 +6,8 @@ package com.javarush.task.task02.task0207;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int a = 1, b = 2;
+        System.out.println(a + b);
+        System.out.println(a * b);
     }
 }
