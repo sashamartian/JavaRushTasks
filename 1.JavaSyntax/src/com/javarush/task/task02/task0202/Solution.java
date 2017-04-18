ecc3aa45f50b25f774f055783c1637b7cd4318b7
sashamartian <sasha.martian@yandex.ru>
2017-04-18 19:02:02 +0700

    com.javarush.task.task02.task0202 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
@@ -6,9 +6,14 @@ package com.javarush.task.task02.task0202;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Person person = new Person();
     }
 
     public static class Person {
         //напишите тут ваш код
+        String name;
+        int age;
+        int weight;
+        int money;
     }
 }
