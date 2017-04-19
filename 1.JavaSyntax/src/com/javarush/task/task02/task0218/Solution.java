13c2ed6547d3564247dae376c92753ce944bed91
sashamartian <sasha.martian@yandex.ru>
2017-04-19 12:32:16 +0700

    com.javarush.task.task02.task0218 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0218/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0218/Solution.java
@@ -6,7 +6,9 @@ package com.javarush.task.task02.task0218;
 public class Solution {
     public static void print3(String s) {
         //напишите тут ваш код
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
