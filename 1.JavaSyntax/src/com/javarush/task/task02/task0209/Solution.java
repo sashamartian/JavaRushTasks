49bba90c3a21c69dfa13413794f53ba6fcb1ad0c
sashamartian <sasha.martian@yandex.ru>
2017-04-19 11:11:29 +0700

    com.javarush.task.task02.task0209 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0209/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0209/Solution.java
@@ -9,6 +9,11 @@ public class Solution {
         dog1.name = "Max";
 
         //напишите тут ваш код
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
     }
 
     public static class Dog {
