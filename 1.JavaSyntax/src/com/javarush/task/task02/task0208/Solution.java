2bad85268500b4f28102cd68b641b1356534172f
sashamartian <sasha.martian@yandex.ru>
2017-04-19 11:09:34 +0700

    com.javarush.task.task02.task0208 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0208/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0208/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.task02.task0208;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
     }
 
     public static class Cat {
