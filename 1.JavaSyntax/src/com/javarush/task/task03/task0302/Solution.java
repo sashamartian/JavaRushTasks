42f660f655172815c4fafefff25c45873f7f1ced
sashamartian <sasha.martian@yandex.ru>
2017-04-19 19:44:46 +0700

    com.javarush.task.task03.task0302 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0302/Solution.java
@@ -5,6 +5,9 @@ package com.javarush.task.task03.task0302;
 */
 public class Solution {
     //напишите тут ваш код
+    public static void printString(String s) {
+        System.out.println(s);
+    }
 
     public static void main(String[] args) {
         printString("Hello Amigo!");
