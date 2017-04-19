749ee16b6c3dae68842fe6a1dfd05119f95bc92b
sashamartian <sasha.martian@yandex.ru>
2017-04-19 12:30:25 +0700

    com.javarush.task.task02.task0217 Done

--- 1.JavaSyntax/src/com/javarush/task/task02/task0217/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0217/Solution.java
@@ -6,12 +6,16 @@ package com.javarush.task.task02.task0217;
 public class Solution {
     public static int min(int a, int b, int c, int d) {
         //напишите тут ваш код
-
+        int x = min(a, b);
+        int y = min(c, d);
+        return min(x, y);
     }
 
     public static int min(int a, int b) {
         //напишите тут ваш код
-
+        if (a < b)
+            return a;
+        return b;
     }
 
     public static void main(String[] args) throws Exception {
