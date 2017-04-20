2663595324fa3d509bec5ee3058f7cc3143105c1
sashamartian <sasha.martian@yandex.ru>
2017-04-20 11:47:28 +0700

    com.javarush.task.task03.task0307 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
@@ -7,6 +7,35 @@ package com.javarush.task.task03.task0307;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Zerg zerg01 = new Zerg();
+        Zerg zerg02 = new Zerg();
+        Zerg zerg03 = new Zerg();
+        Zerg zerg04 = new Zerg();
+        Zerg zerg05 = new Zerg();
+        Zerg zerg06 = new Zerg();
+        Zerg zerg07 = new Zerg();
+        Zerg zerg08 = new Zerg();
+        Zerg zerg09 = new Zerg();
+        Zerg zerg10 = new Zerg();
+
+        Terran terran01 = new Terran();
+        Terran terran02 = new Terran();
+        Terran terran03 = new Terran();
+        Terran terran04 = new Terran();
+        Terran terran05 = new Terran();
+
+        Protoss protoss01 = new Protoss();
+        Protoss protoss02 = new Protoss();
+        Protoss protoss03 = new Protoss();
+        Protoss protoss04 = new Protoss();
+        Protoss protoss05 = new Protoss();
+        Protoss protoss06 = new Protoss();
+        Protoss protoss07 = new Protoss();
+        Protoss protoss08 = new Protoss();
+        Protoss protoss09 = new Protoss();
+        Protoss protoss10 = new Protoss();
+        Protoss protoss11 = new Protoss();
+        Protoss protoss12 = new Protoss();
     }
 
     public static class Zerg {
