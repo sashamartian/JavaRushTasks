a4681672824c36ef31754436244ecc822088d8d2
sashamartian <sasha.martian@yandex.ru>
2017-04-20 11:53:23 +0700

    com.javarush.task.task03.task0309 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
@@ -7,5 +7,13 @@ package com.javarush.task.task03.task0309;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int res;
+        for (int i = 1; i <= 10; i++) {
+            res = 0;
+            for (int j = 1; j <= i; j++) {
+                res = res + j;
+            }
+            System.out.println(res);
+        }
     }
 }
