73f9d737543f1a427d152d350dd5d5657157a5e7
sashamartian <sasha.martian@yandex.ru>
2017-04-20 11:43:11 +0700

    com.javarush.task.task03.task0305 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -7,5 +7,6 @@ package com.javarush.task.task03.task0305;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("MAY 1 1912");
     }
 }
