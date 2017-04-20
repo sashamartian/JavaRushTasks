47e1977f7ae57042e89b8eddd96d85f39f09681a
sashamartian <sasha.martian@yandex.ru>
2017-04-20 12:02:02 +0700

    com.javarush.task.task03.task0312 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
@@ -6,8 +6,13 @@ package com.javarush.task.task03.task0312;
 
 public class Solution {
     //напишите тут ваш код
+    public static int convertToSeconds(int hour) {
+        return hour * 3600;
+    }
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println(convertToSeconds(1));
+        System.out.println(convertToSeconds(2));
     }
 }
