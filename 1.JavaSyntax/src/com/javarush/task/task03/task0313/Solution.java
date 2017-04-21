bcf25cf2aa55d6e1c55d68d48ac8a14cf1d984a4
sashamartian <sasha.martian@yandex.ru>
2017-04-21 17:44:47 +0700

    com.javarush.task.task03.task0313 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -7,5 +7,11 @@ package com.javarush.task.task03.task0313;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("МамаМылаРаму");
+        System.out.println("МылаМамаРаму");
+        System.out.println("МылаРамуМама");
+        System.out.println("МамаРамуМыла");
+        System.out.println("РамуМамаМыла");
+        System.out.println("РамуМылаМама");
     }
 }
