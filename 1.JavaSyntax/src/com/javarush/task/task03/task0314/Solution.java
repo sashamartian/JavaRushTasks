ddeee68fe4679fd20ade2b110532dd5fe75e5f90
sashamartian <sasha.martian@yandex.ru>
2017-04-22 15:57:10 +0700

    com.javarush.task.task03.task0314 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -7,5 +7,15 @@ package com.javarush.task.task03.task0314;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int i = 1, j;
+        while (i <= 10) {
+            j = 1;
+            while (j <= 10) {
+                System.out.print((i * j) + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
     }
 }
