90bff9484136ab36c26aaebe03981c9ce0562119
sashamartian <sasha.martian@yandex.ru>
2017-04-22 16:04:21 +0700

    com.javarush.task.task03.task0316 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -7,5 +7,7 @@ package com.javarush.task.task03.task0316;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("C:\\Program Files\\Java\\jdk1.7.0\\bin");
+        System.out.println("C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\");
     }
 }
