1c288a14c4eab05946f570efd4399728c2cd3e80
sashamartian <sasha.martian@yandex.ru>
2017-04-23 15:07:28 +0700

    com.javarush.task.task03.task0324 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -7,5 +7,15 @@ package com.javarush.task.task03.task0324;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
     }
 }
