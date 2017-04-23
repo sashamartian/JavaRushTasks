1ea1cff1b0fe0b0382f5de73c3ea14436e105b95
sashamartian <sasha.martian@yandex.ru>
2017-04-23 13:48:16 +0700

    com.javarush.task.task03.task0319 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -9,5 +9,10 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        System.out.println(name + " получает " + num1 + " через " + num2 + " лет.");
     }
 }
