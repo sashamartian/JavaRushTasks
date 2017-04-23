db4df111589e0ebfb844362a4f815f001814cf7c
sashamartian <sasha.martian@yandex.ru>
2017-04-23 14:09:22 +0700

    com.javarush.task.task03.task0322 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
@@ -10,5 +10,13 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(
+                br.readLine() +
+                " + " +
+                br.readLine() +
+                " + " +
+                br.readLine() +
+                " = Чистая любовь, да-да!");
     }
 }
