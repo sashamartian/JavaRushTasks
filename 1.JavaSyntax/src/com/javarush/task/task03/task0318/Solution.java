1361a4c4ddb4b3fa427fa5d8804cce2adcfbbc91
sashamartian <sasha.martian@yandex.ru>
2017-04-23 13:44:55 +0700

    com.javarush.task.task03.task0318 Done

--- 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -9,5 +9,9 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        String name = br.readLine();
+        System.out.println(name + " захватит мир через " + year + " лет. Му-ха-ха!");
     }
 }
