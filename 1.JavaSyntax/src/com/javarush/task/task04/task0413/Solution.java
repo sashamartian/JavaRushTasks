fe6a2f2452daae1851bdad4aa6f713c338e023a3
sashamartian <sasha.martian@yandex.ru>
2017-05-04 12:21:39 +0700

    com.javarush.task.task04.task0413 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0413/Solution.java
@@ -9,5 +9,32 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer day = Integer.parseInt(br.readLine());
+
+        if (day == 1) {
+            System.out.println("понедельник");
+        }
+        else if (day == 2) {
+            System.out.println("вторник");
+        }
+        else if (day == 3) {
+            System.out.println("среда");
+        }
+        else if (day == 4) {
+            System.out.println("четверг");
+        }
+        else if (day == 5) {
+            System.out.println("пятница");
+        }
+        else if (day == 6) {
+            System.out.println("суббота");
+        }
+        else if (day == 7) {
+            System.out.println("воскресенье");
+        }
+        else {
+            System.out.println("такого дня недели не существует");
+        }
     }
 }
