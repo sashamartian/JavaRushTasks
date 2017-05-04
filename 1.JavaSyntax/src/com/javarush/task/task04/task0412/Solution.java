8cd29e5a36004aaa37343339385f0744f3a26423
sashamartian <sasha.martian@yandex.ru>
2017-05-04 12:16:20 +0700

    com.javarush.task.task04.task0412 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -9,7 +9,17 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(System.in));
+        Integer in = Integer.parseInt(bufferedReader.readLine());
 
+        if (in > 0) {
+            System.out.println(in * 2);
+        } else if (in < 0) {
+            System.out.println(in + 1);
+        } else {
+            System.out.println(in);
+        }
     }
 
 }
