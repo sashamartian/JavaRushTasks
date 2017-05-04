f106b7214280a3abc95c95d95a92a26b51caa013
sashamartian <sasha.martian@yandex.ru>
2017-05-04 15:41:17 +0700

    com.javarush.task.task04.task0416 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -9,5 +9,18 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Double minute = Double.parseDouble(br.readLine());
+        Double ostatok = minute % 5;
+
+        if (ostatok >= 0 && ostatok < 3) {
+            System.out.println("зелёный");
+        }
+        else if (ostatok >= 3 && ostatok < 4) {
+            System.out.println("желтый");
+        }
+        else {
+            System.out.println("красный");
+        }
     }
 }
