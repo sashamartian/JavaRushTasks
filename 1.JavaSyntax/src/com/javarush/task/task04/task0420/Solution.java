469ffe6fa755ad232d51c0c19b59a0313f53435b
sashamartian <sasha.martian@yandex.ru>
2017-05-07 16:03:00 +0700

    com.javarush.task.task04.task0420 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -9,5 +9,38 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int num1 = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+        int num2 = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+        int num3 = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        int n;
+
+        if (num2 > num1) {
+            n = num1;
+            num1 = num2;
+            num2 = n;
+        }
+
+        if (num3 > num2) {
+            n = num2;
+            num2 = num3;
+            num3 = n;
+        }
+
+        if (num2 > num1) {
+            n = num1;
+            num1 = num2;
+            num2 = n;
+        }
+
+        if (num3 > num2) {
+            n = num2;
+            num2 = num3;
+            num3 = n;
+        }
+
+        System.out.println(num1 + " " + num2 + " " + num3);
     }
 }
