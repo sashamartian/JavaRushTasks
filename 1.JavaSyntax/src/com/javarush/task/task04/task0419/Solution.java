60f5da4859141076c8095415b6673b081d096f44
sashamartian <sasha.martian@yandex.ru>
2017-05-07 15:38:58 +0700

    com.javarush.task.task04.task0419 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -9,5 +9,22 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+
+        int x = max(a, b);
+        int y = max(c, d);
+
+        System.out.println(max(x, y));
+    }
+
+    public static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        }
+        return y;
     }
 }
