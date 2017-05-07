6d6a8a692e66824aef0f41215408b41c487ecd02
sashamartian <sasha.martian@yandex.ru>
2017-05-07 16:10:03 +0700

    com.javarush.task.task04.task0423 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
@@ -9,5 +9,13 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine(); //читаем строку с клавиатуры
+        int age = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        if (age > 20) {
+            System.out.println("И 18-ти достаточно");
+        }
     }
 }
