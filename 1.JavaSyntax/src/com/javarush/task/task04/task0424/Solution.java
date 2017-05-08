1781efe8c2e234ad851418ffccdfaa96965d29f2
sashamartian <sasha.martian@yandex.ru>
2017-05-08 11:21:06 +0700

    com.javarush.task.task04.task0424 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -9,5 +9,21 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int num3 = Integer.parseInt(br.readLine());
+
+        if (num1 != num2 && num1 != num3) {
+            System.out.println(1);
+        }
+
+        if (num2 != num1 && num2 != num3) {
+            System.out.println(2);
+        }
+
+        if (num3 != num2 && num3 != num1) {
+            System.out.println(3);
+        }
     }
 }
