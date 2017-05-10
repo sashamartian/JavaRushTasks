83b9dbc52e69ca9316f0dc20547ae88198cd358b
sashamartian <sasha.martian@yandex.ru>
2017-05-10 17:32:43 +0700

    com.javarush.task.task04.task0427 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
@@ -9,6 +9,22 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        int a = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        if (a > 0 && a < 10 && a % 2 == 0) {
+            System.out.println("четное однозначное число");
+        } else if (a > 0 && a < 10 && a % 2 != 0) {
+            System.out.println("нечетное однозначное число");
+        } else if (a > 9 && a < 100 && a % 2 == 0) {
+            System.out.println("четное двузначное число");
+        } else if (a > 9 && a < 100 && a % 2 != 0) {
+            System.out.println("нечетное двузначное число");
+        } else if (a > 99 && a < 1000 && a % 2 == 0) {
+            System.out.println("четное трехзначное число");
+        } else if (a > 99 && a < 1000 && a % 2 != 0) {
+            System.out.println("нечетное трехзначное число");
+        }
     }
 }
