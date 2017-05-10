e470a720c2646bfc125f53100802f1667d4c5bc8
sashamartian <sasha.martian@yandex.ru>
2017-05-10 17:35:32 +0700

    com.javarush.task.task04.task0429 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
@@ -9,6 +9,34 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        int count_minus = 0;
+        int count_plus = 0;
+
+        int a = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+        int b = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+        int c = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        if (a >= 0) {
+            ++count_plus;
+        } else {
+            ++count_minus;
+        }
+
+        if (b >= 0) {
+            ++count_plus;
+        } else {
+            ++count_minus;
+        }
+
+        if (c >= 0) {
+            ++count_plus;
+        } else {
+            ++count_minus;
+        }
+
+        System.out.println("количество отрицательных чисел: " + count_minus);
+        System.out.println("количество положительных чисел: " + count_plus);
     }
 }
