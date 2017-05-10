7340b37a32919f23331326061b6992e4edf0e13f
sashamartian <sasha.martian@yandex.ru>
2017-05-10 19:49:52 +0700

    com.javarush.task.task04.task0431 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0431/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0431/Solution.java
@@ -9,6 +9,10 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            --i;
+        }
     }
 }
