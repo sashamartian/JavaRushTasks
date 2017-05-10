801f7193e5f2cc39d530dffbbb987fd81399ffb9
sashamartian <sasha.martian@yandex.ru>
2017-05-10 17:31:12 +0700

    com.javarush.task.task04.task0426 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -9,5 +9,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        if (a < 0 && a % 2 == 0) {
+            System.out.println("отрицательное четное число");
+        } else if (a < 0 && a % 2 != 0) {
+            System.out.println("отрицательное нечетное число");
+        } else if (a > 0 && a % 2 == 0) {
+            System.out.println("положительное четное число");
+        } else if (a > 0 && a % 2 != 0) {
+            System.out.println("положительное нечетное число");
+        } else {
+            System.out.println("ноль");
+        }
     }
 }
