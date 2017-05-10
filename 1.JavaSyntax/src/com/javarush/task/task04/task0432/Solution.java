2aa708e2a8a0ba1d425a40acc66f033e35527052
sashamartian <sasha.martian@yandex.ru>
2017-05-10 19:51:11 +0700

    com.javarush.task.task04.task0432 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
@@ -11,6 +11,14 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String str = reader.readLine(); //читаем строку с клавиатуры
+        int count = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        while (count > 0) {
+            System.out.println(str);
+            --count;
+        }
     }
 }
