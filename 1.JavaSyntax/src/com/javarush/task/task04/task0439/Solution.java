7a314c66bebd5f188e6aa63e289412d48e98b19f
sashamartian <sasha.martian@yandex.ru>
2017-05-11 17:22:57 +0700

    com.javarush.task.task04.task0439 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
@@ -9,6 +9,12 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String name = reader.readLine(); //читаем строку с клавиатуры
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + " любит меня.");
+        }
     }
 }
