56d04cf432655a6e221b06085f91829cf38b3c81
sashamartian <sasha.martian@yandex.ru>
2017-05-11 17:19:15 +0700

    com.javarush.task.task04.task0436 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
@@ -10,6 +10,16 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        int m = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+        int n = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
