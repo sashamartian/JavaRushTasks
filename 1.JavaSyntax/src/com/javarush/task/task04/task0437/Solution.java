d1b465132436ded6fa6a07b9cb5726f5c44e31c1
sashamartian <sasha.martian@yandex.ru>
2017-05-11 17:20:29 +0700

    com.javarush.task.task04.task0437 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
@@ -8,6 +8,11 @@ package com.javarush.task.task04.task0437;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
