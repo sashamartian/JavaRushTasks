a562c065e5212fb12e2929a8fcfcf64edad36010
sashamartian <sasha.martian@yandex.ru>
2017-05-11 17:17:54 +0700

    com.javarush.task.task04.task0435 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -7,6 +7,9 @@ package com.javarush.task.task04.task0435;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-
+        for (int i = 1; i < 101; ++i) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
     }
 }
