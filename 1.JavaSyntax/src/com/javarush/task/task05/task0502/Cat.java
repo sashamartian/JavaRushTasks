f82f1828150b647c2b18511ec94b67ffdcf7827d
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:29:44 +0700

    com.javarush.task.task05.task0502 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -15,9 +15,27 @@ public class Cat {
 
     public boolean fight(Cat anotherCat) {
         //напишите тут ваш код
+        if (this.age * this.weight * this.strength > anotherCat.age * anotherCat.weight * anotherCat.strength) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
+        Cat catVaska = new Cat();
+        catVaska.name = "Vaska";
+        catVaska.age = 1;
+        catVaska.weight = 1;
+        catVaska.strength = 1;
 
+        Cat catBarsik = new Cat();
+        catVaska.name = "Barsik";
+        catVaska.age = 2;
+        catVaska.weight = 2;
+        catVaska.strength = 2;
+
+        System.out.println(catVaska.fight(catBarsik));
+        System.out.println(catBarsik.fight(catVaska));
     }
 }
