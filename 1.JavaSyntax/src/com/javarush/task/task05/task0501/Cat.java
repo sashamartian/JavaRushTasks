356f64434f66e3cf2fc5991c95e01129350b70bb
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:28:22 +0700

    com.javarush.task.task05.task0501 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -6,6 +6,8 @@ package com.javarush.task.task05.task0501;
 
 public class Cat {
     //напишите тут ваш код
+    String name;
+    int age, weight, strength;
 
     public static void main(String[] args) {
 
