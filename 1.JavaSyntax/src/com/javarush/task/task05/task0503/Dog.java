474bc642f158a3088660af58e834a064de56c288
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:31:14 +0700

    com.javarush.task.task05.task0503 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -7,6 +7,28 @@ package com.javarush.task.task05.task0503;
 
 public class Dog {
     //напишите тут ваш код
+    String name;
+    int age;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
 
     public static void main(String[] args) {
 
