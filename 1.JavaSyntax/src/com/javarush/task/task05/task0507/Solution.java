ba6e916e67694291148b863205ae2db729bef253
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:56:18 +0700

    com.javarush.task.task05.task0507 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,22 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Float input = Float.parseFloat(reader.readLine());
+        Float summ = 0f;
+        Float count = 0f;
+        while (input != -1) {
+            summ += input;
+            count++;
+            input = Float.parseFloat(reader.readLine());
+        }
+        System.out.println(summ / count);
     }
 }
 
