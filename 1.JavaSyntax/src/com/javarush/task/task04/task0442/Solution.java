99f9a3a8e1bf652d7120e88b3b854310a06d69ef
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:23:40 +0700

    com.javarush.task.task04.task0442 Done

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -10,5 +10,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int num = 0;
+
+        while (true) {
+            num = Integer.parseInt(reader.readLine()); //читаем строку с клавиатуры
+
+            sum += num;
+
+            if (num == -1)
+                break;
+        }
+
+        System.out.println(sum);
     }
 }
