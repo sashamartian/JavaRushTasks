daad2ca01daec5f11aebf847914cd456d62eecdd
sashamartian <sasha.martian@yandex.ru>
2017-05-15 14:41:32 +0700

    com.javarush.task.task05.task0506 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
@@ -6,6 +6,9 @@ package com.javarush.task.task05.task0506;
 
 public class Person {
     //напишите тут ваш код
+    String name, address;
+    int age;
+    char sex;
 
     public static void main(String[] args) {
 
