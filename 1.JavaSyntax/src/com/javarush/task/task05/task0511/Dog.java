329724becfd6d06f58c42501fe9bb0b3e284e287
sashamartian <sasha.martian@yandex.ru>
2017-05-17 15:51:51 +0700

    com.javarush.task.task05.task0511 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0511/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0511/Dog.java
@@ -6,8 +6,26 @@ package com.javarush.task.task05.task0511;
 
 public class Dog {
     //напишите тут ваш код
+    String name, color, address;
+    int hight;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int hight) {
+        this.name = name;
+        this.hight = hight;
+    }
+
+    public void initialize(String name, int hight, String color) {
+        this.name = name;
+        this.hight = hight;
+        this.color = color;
+    }
 
     public static void main(String[] args) {
-
+        Dog dog = new Dog();
+        dog.initialize("Gav");
     }
 }
