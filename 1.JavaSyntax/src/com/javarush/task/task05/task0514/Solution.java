a97640f67470102e448b53897dc9082a54553266
sashamartian <sasha.martian@yandex.ru>
2017-05-17 16:06:27 +0700

    com.javarush.task.task05.task0514 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
@@ -7,9 +7,18 @@ package com.javarush.task.task05.task0514;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Person person = new Person();
+        person.initialize("user", 25);
     }
 
     static class Person {
         //напишите тут ваш код
+        public String name;
+        public int age;
+
+        public void initialize(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
