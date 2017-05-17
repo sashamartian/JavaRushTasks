0f6260fe554a11490dbf841d6fb23bf758bbcdf0
sashamartian <sasha.martian@yandex.ru>
2017-05-17 15:49:28 +0700

    com.javarush.task.task05.task0509 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0509/Friend.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0509/Friend.java
@@ -6,6 +6,24 @@ package com.javarush.task.task05.task0509;
 
 public class Friend {
     //напишите тут ваш код
+    String name;
+    int age;
+    boolean sex;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, boolean sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
     public static void main(String[] args) {
 
