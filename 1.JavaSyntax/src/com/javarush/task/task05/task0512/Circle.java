95cbac280349ffddd32347df745181630abc3c12
sashamartian <sasha.martian@yandex.ru>
2017-05-17 15:52:59 +0700

    com.javarush.task.task05.task0512 Done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0512/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0512/Circle.java
@@ -6,8 +6,32 @@ package com.javarush.task.task05.task0512;
 
 public class Circle {
     //напишите тут ваш код
+    String color;
+    int centerX, centerY, radius, width;
+
+    public void initialize(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public void initialize(int centerX, int centerY, int radius, int width) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+    }
+
+    public void initialize(int centerX, int centerY, int radius, int width, String color) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+        this.color = color;
+    }
 
     public static void main(String[] args) {
-
+        Circle circle = new Circle();
+        circle.initialize(1, 1, 1);
     }
 }
